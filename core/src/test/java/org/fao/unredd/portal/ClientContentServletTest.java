2cc38c30859f9125f72b9ba14311c5a03ddf3e55
Fernando González Cortés <fernando.gonzalez@geomati.co>
2016-09-16 10:56:38 +0200

    test javadoc

--- core/src/test/java/org/fao/unredd/portal/ClientContentServletTest.java
+++ core/src/test/java/org/fao/unredd/portal/ClientContentServletTest.java
@@ -41,16 +41,23 @@ public class ClientContentServletTest {
 		MockitoAnnotations.initMocks(this);
 	}
 
+	/**
+	 * Mock ServletContext and initialize {@link AppContextListener} with it.
+	 * Capture {@link Config} instance.
+	 * 
+	 * @param folder
+	 */
 	public void setupConfigurationFolder(String folder) {
 		AppContextListener listener = new AppContextListener();
-		ServletContextEvent servletContextEvent = mock(ServletContextEvent.class);
+		ServletContextEvent servletContextEvent = mock(
+				ServletContextEvent.class);
 		ServletContext servletContext = mock(ServletContext.class);
-		when(servletContext.getInitParameter("PORTAL_CONFIG_DIR")).thenReturn(
-				folder);
-		when(servletContext.getResourcePaths("/WEB-INF/lib")).thenReturn(
-				new HashSet<String>());
-		when(servletContext.getRealPath("/WEB-INF/classes/")).thenReturn(
-				folder + "/WEB-INF/classes");
+		when(servletContext.getInitParameter("PORTAL_CONFIG_DIR"))
+				.thenReturn(folder);
+		when(servletContext.getResourcePaths("/WEB-INF/lib"))
+				.thenReturn(new HashSet<String>());
+		when(servletContext.getRealPath("/WEB-INF/classes/"))
+				.thenReturn(folder + "/WEB-INF/classes");
 		when(servletContextEvent.getServletContext())
 				.thenReturn(servletContext);
 		listener.contextInitialized(servletContextEvent);
@@ -77,15 +84,23 @@ public class ClientContentServletTest {
 		requirePaths("/testJavaRootSubfolders");
 	}
 
-	private void requirePaths(String classpathPrefix) throws ServletException,
-			IOException {
+	/**
+	 * Get descriptors from config instance and query all modules, styles and
+	 * libs to the {@link ClientContentServlet}
+	 * 
+	 * @param classpathPrefix
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void requirePaths(String classpathPrefix)
+			throws ServletException, IOException {
 		Config config = configCaptor.getValue();
 
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		when(request.getSession()).thenReturn(mock(HttpSession.class));
 
-		PluginDescriptors pluginDescriptors = config.getPluginConfig(
-				Locale.ROOT, request);
+		PluginDescriptors pluginDescriptors = config
+				.getPluginConfig(Locale.ROOT, request);
 
 		List<String> modules = new ArrayList<>();
 		List<String> styles = new ArrayList<>();
@@ -148,8 +163,8 @@ public class ClientContentServletTest {
 		servlet.setTestingClasspathRoot("/testNoJavaPlugins/WEB-INF/classes/");
 		ServletConfig servletConfig = mock(ServletConfig.class);
 		ServletContext servletContext = mock(ServletContext.class);
-		when(servletContext.getAttribute("config")).thenReturn(
-				configCaptor.getValue());
+		when(servletContext.getAttribute("config"))
+				.thenReturn(configCaptor.getValue());
 		when(servletConfig.getServletContext()).thenReturn(servletContext);
 		servlet.init(servletConfig);
 
