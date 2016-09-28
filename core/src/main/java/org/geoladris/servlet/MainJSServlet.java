015f9daa06ba275fcd5e6af2151d176b718e5250
Víctor González <victor.gonzalez@geomati.co>
2016-09-28 14:08:00 +0200

    Cache main.js only if NFMS_CONFIG_CACHE is true. Fixes #27

--- core/src/main/java/org/geoladris/servlet/MainJSServlet.java
+++ core/src/main/java/org/geoladris/servlet/MainJSServlet.java
@@ -23,9 +23,25 @@ public class MainJSServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+    boolean useCache = Boolean.parseBoolean(System.getenv(AppContextListener.ENV_CONFIG_CACHE));
+    doGet(req, resp, useCache);
+  }
 
-    if (output == null) {
-
+  /**
+   * Only for testing purposes.
+   * 
+   * TODO: Fix when we have a consistent configuration variable management
+   * (https://github.com/geoladris/core/issues/28)
+   * 
+   * @param req
+   * @param resp
+   * @param useCache
+   * @throws ServletException
+   * @throws IOException
+   */
+  void doGet(HttpServletRequest req, HttpServletResponse resp, boolean useCache)
+      throws ServletException, IOException {
+    if (!useCache || output == null) {
       Config config = (Config) getServletContext().getAttribute(AppContextListener.ATTR_CONFIG);
       Locale locale = (Locale) req.getAttribute(LangFilter.ATTR_LOCALE);
       PluginDescriptors pluginDescriptors = config.getPluginConfig(locale, req);
@@ -44,5 +60,4 @@ public class MainJSServlet extends HttpServlet {
 
     resp.getWriter().print(output);
   }
-
 }
