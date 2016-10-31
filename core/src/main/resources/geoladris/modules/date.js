cce0d3ca3d5047a016d9ff10af0419e5dedb84b1
Víctor González <victor.gonzalez@geomati.co>
2016-10-31 13:38:06 +0100

    Fix date parsing depending on locale

--- core/src/main/resources/geoladris/modules/date.js
+++ core/src/main/resources/geoladris/modules/date.js
@@ -4,25 +4,25 @@ define(["i18n"], function(i18n) {
 				+ "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
 		var d = str.match(new RegExp(regexp));
 		if (d) {
-			var date = new Date(d[1], 0, 1), offset = 0, time;
+			var date = new Date(Date.UTC(d[1], 0, 1)), offset = 0, time;
 
 			if (d[3]) {
-				date.setMonth(d[3] - 1);
+				date.setUTCMonth(d[3] - 1);
 			}
 			if (d[5]) {
-				date.setDate(d[5]);
+				date.setUTCDate(d[5]);
 			}
 			if (d[7]) {
-				date.setHours(d[7]);
+				date.setUTCHours(d[7]);
 			}
 			if (d[8]) {
-				date.setMinutes(d[8]);
+				date.setUTCMinutes(d[8]);
 			}
 			if (d[10]) {
-				date.setSeconds(d[10]);
+				date.setUTCSeconds(d[10]);
 			}
 			if (d[12]) {
-				date.setMilliseconds(Number("0." + d[12]) * 1000);
+				date.setUTCMilliseconds(Number("0." + d[12]) * 1000);
 			}
 			if (d[14]) {
 				offset = (Number(d[16]) * 60) + Number(d[17]);
