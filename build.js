6c46967c97010cd004aa1c5f116c2f7333d893c7
Víctor González <victor.gonzalez@geomati.co>
2017-04-04 12:50:37 +0200

    Move 'js-test' to 'test'

--- build.js
+++ build.js
@@ -1,25 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-const JSLIB = "core/js/jslib/";
-const TEST_LIB = "core/js-test/jslib/";
+const JSLIB = 'core/js/jslib/';
+const TEST_LIB = 'core/test/jslib/';
 
 function copy(lib, dest) {
-  var input = fs.createReadStream(path.join("node_modules", lib));
-  var output = fs.createWriteStream(path.join(dest, path.basename(lib)));
-  input.pipe(output);
+	if (!fs.existsSync(dest)) {
+		fs.mkdirSync(dest);
+	}
+	var input = fs.createReadStream(path.join('node_modules', lib));
+	var output = fs.createWriteStream(path.join(dest, path.basename(lib)));
+	input.pipe(output);
 }
 
-try {
-  fs.mkdirSync(JSLIB);
-} catch (err) {
-}
-copy("jquery/dist/jquery.min.js", JSLIB);
-copy("requirejs/require.js", JSLIB);
+// dependencies
+copy('jquery/dist/jquery.min.js', JSLIB);
+copy('requirejs/require.js', JSLIB);
 
-try {
-  fs.mkdirSync(TEST_LIB);
-} catch (err) {
-}
-copy("squirejs/src/Squire.js", TEST_LIB);
-
-
+// test dependencies
+copy('squirejs/src/Squire.js', TEST_LIB);
