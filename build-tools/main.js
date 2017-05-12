8aa5de76bb1d444a5bdd4127cd88f5660fa86a6e
Víctor González <victor.gonzalez@geomati.co>
2017-05-12 20:05:37 +0200

    Remove unused npmResources script

--- build-tools/main.js
+++ build-tools/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const resources = require('./npmResources');
-const build = require('./buildApp');
-
 module.exports = {
-	copyToLib: resources.copyToLib,
-	process: resources.process,
-	build: build
+	build: require('./buildApp')
 };
