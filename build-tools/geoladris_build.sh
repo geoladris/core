af75031405f32c9aa424b8a3719106112328eb47
Víctor González <victor.gonzalez@geomati.co>
2017-08-22 10:44:26 +0200

    Fix and improve geoladris_build.sh

--- build-tools/geoladris_build.sh
+++ build-tools/geoladris_build.sh
@@ -2,29 +2,31 @@
 
 set -e
 
-GEOLADRIS_VERSION="6.0-SNAPSHOT"
+geoladrisVersion="7.0.0-SNAPSHOT"
 
 USAGE="""
-usage: $0 [-d <dir>] [-s]
+usage: $0 [-d <dir>] [-v geoladris_version] [-s]
 
 Options:
-  -d <dir>  Directory to use for building. It must contain a build.json
-            descriptor and a default_config directory; default is the
-            current directory
-  -s        Skips deployment; default is false. Note that in order to deploy,
-            not only this switch must not be used, but also the
-            distribution_management section in build.json must be valid.
+  -d <dir>      Directory to use for building. It must contain a build.json
+                descriptor and a default_config directory; default is the
+                current directory
+  -p            Performs deployment; default is false. Note that in order to 
+                deploy, not only this switch must be used, but also the
+                distribution_management section in build.json must be valid.
+  -v <version>  Geoladris version for plugins without a version; default is
+                ${geoladrisVersion}
 
 build.json must contain a JSON object with the following properties:
 
   - group                    Artifact group, only required for deploying;
                              default is 'org.geoladris'.
   - name                     Application's name; default is 'demo'.
-  - version                  Application's version; default is '1.0-SNAPSHOT'.
+  - version                  Application's version; default is '1.0.0-SNAPSHOT'.
   - plugins                  An array of strings; each string has this format
                              [groupId:]pluginId[:version].
-                             groupId is optional; default is 'org.fao.unredd'
-                             version is optional; default is '${GEOLADRIS_VERSION}'
+                             groupId is optional; default is geoladris group ids.
+                             version is optional; default is '${geoladrisVersion}'
   - repositories             A JSON object containing extra repositories for
                              plugins. Keys are repository names; values are
                              repository URLs.
@@ -66,17 +68,20 @@ Complete example:
 }
 """
 
-skipDeploy="false"
+deploy="false"
 dir=$PWD
 
-while getopts ":hsd:" opt; do
+while getopts ":hpv:d:" opt; do
   case $opt in
     h)
       echo "$USAGE"
       exit 0
       ;;
     s)
-      skipDeploy="true"
+      deploy="true"
+      ;;
+    v)
+      geoladrisVersion="$OPTARG"
       ;;
     d)
       dir="$OPTARG"
@@ -91,18 +96,21 @@ done
 buildJson="$dir/build.json"
 defaultConfig="$dir/default_config"
 workDir="$dir/.geoladris"
+log="$dir/geoladris.log"
 
 if [ ! -f "${buildJson}" ]; then
   echo "$dir must be a directory containing a build.json file"
   exit 1
 fi
 
-mvn -version
+
+mvn -version > $log
+echo >> $log
+
+echo "[INFO] Generating directories..."
 
 rm -rf ${workDir}
 mkdir ${workDir}
-mkdir -p "${workDir}/src/main/config"
-mkdir -p "${workDir}/src/main/webapp/optimized"
 mkdir -p "${workDir}/src/main/webapp/WEB-INF"
 
 if [ -d "${defaultConfig}" ]; then
@@ -119,15 +127,16 @@ function getOpt {
 }
 
 name=`getOpt ".name" "demo"`
-version=`getOpt ".version" "1.0-SNAPSHOT"`
+version=`getOpt ".version" "1.0.0-SNAPSHOT"`
 group=`getOpt ".group" "org.geoladris"`
 plugins=`jq -r '.plugins[]' ${buildJson} 2> /dev/null`
 
 if [ -z "${plugins}" ]; then
-  echo "build.json must have a 'plugins' property with an array containing at least one plugin"
+  echo "[ERROR] build.json must have a 'plugins' property with an array containing at least one plugin"
   exit 2
 fi
 
+echo "[INFO] Generating Java dependency files..."
 ###########
 # pom.xml #
 ###########
@@ -144,17 +153,35 @@ cat >> ${pom} << EOF
 	<version>${version}</version>
 	<packaging>war</packaging>
 
+	<properties>
+		<project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+		<!-- See https://nodejs.org/en/download/ for latest node version -->
+		<node.version>v6.10.1</node.version>
+		<!-- See https://yarnpkg.com/ for latest yarn version -->
+		<yarn.version>v0.23.2</yarn.version>
+	</properties>
+
 	<repositories>
-	  <repository>
-	    <id>geoladris-releases</id>
-	    <url>http://nullisland.geomati.co:8082/repository/releases/</url>
-	  </repository>
-	  <repository>
-	    <id>geoladris-snapshots</id>
-	    <url>http://nullisland.geomati.co:8082/repository/snapshots/</url>
-	  </repository>
+		<repository>
+		<id>geoladris-releases</id>
+			<url>http://nullisland.geomati.co:8082/repository/releases/</url>
+			</repository>
+		<repository>
+			<id>geoladris-snapshots</id>
+			<url>http://nullisland.geomati.co:8082/repository/snapshots/</url>
+		</repository>
+		<repository>
+			<id>csgis releases</id>
+			<url>http://service.csgis.de/mvn/repository/releases/</url>
+		</repository>
+		<repository>
+			<id>csgis snapshots</id>
+			<url>http://service.csgis.de/mvn/repository/snapshots/</url>
+		</repository>
+
 EOF
 
+# Extra repositories
 repoNames=`jq -r '.maven_repositories | keys[]' ${buildJson} 2> /dev/null | sed '/null/d'`
 
 for repoName in ${repoNames}; do
@@ -171,15 +198,7 @@ cat >> ${pom} << EOF
 	</repositories>
 EOF
 
-cat >> ${pom} << EOF
-	<pluginRepositories>
-		<pluginRepository>
-			<id>geoladris</id>
-			<url>http://nullisland.geomati.co:8082/repository/releases/</url>
-		</pluginRepository>
-	</pluginRepositories>
-EOF
-
+# Distribution management
 distributionSnapshots=`jq -r '.distribution_management.snapshots' "${buildJson}" 2> /dev/null | sed '/null/d'`
 distributionReleases=`jq -r '.distribution_management.releases' "${buildJson}" 2> /dev/null | sed '/null/d'`
 
@@ -217,10 +236,12 @@ cat >> ${pom} << EOF
 EOF
 fi
 
+# Dependencies
 cat >> ${pom} << EOF
 	<dependencies>
 EOF
 
+set +e
 for plugin in ${plugins}; do
   pluginGroup=`echo ${plugin} | cut -s -d: -f 1`
   pluginName=`echo ${plugin} | cut -s -d: -f 2`
@@ -228,8 +249,12 @@ for plugin in ${plugins}; do
   if [ -z "${pluginGroup}" -o -z "${pluginName}" -o -z "${pluginVersion}" ]; then
     pluginGroup="org.fao.unredd"
     pluginName=${plugin}
-    pluginVersion=${GEOLADRIS_VERSION}
+    pluginVersion=${geoladrisVersion}
   fi
+  echo >> $log
+  echo ">> Checking dependency ${pluginGroup}:${pluginName}:${pluginVersion}" >> $log
+  mvn dependency:get -Dartifact=${pluginGroup}:${pluginName}:${pluginVersion} >> $log
+  if [ $? == 0 ]; then
 cat >> ${pom} << EOF
 		<dependency>
 			<groupId>${pluginGroup}</groupId>
@@ -237,120 +262,109 @@ cat >> ${pom} << EOF
 			<version>${pluginVersion}</version>
 		</dependency>
 EOF
+  else
+    echo "[WARN] Invalid Java dependency: ${pluginGroup}:${pluginName}:${pluginVersion}"
+  fi
 done
+echo >> $log
+set -e
 
+# Build
 cat >> ${pom} << 'EOF'
 	</dependencies>
 	<build>
+		<finalName>${project.artifactId}</finalName>
 		<plugins>
 			<plugin>
-				<groupId>org.apache.maven.plugins</groupId>
-				<artifactId>maven-war-plugin</artifactId>
-				<version>2.3</version>
+				<groupId>com.github.eirslett</groupId>
+				<artifactId>frontend-maven-plugin</artifactId>
+				<version>1.4</version>
+				<executions>
+					<execution>
+						<id>install-node-and-yarn</id>
+						<goals>
+							<goal>install-node-and-yarn</goal>
+						</goals>
+						<configuration>
+							<nodeVersion>${node.version}</nodeVersion>
+							<yarnVersion>${yarn.version}</yarnVersion>
+						</configuration>
+					</execution>
+					<execution>
+						<id>yarn install</id>
+						<goals>
+							<goal>yarn</goal>
+						</goals>
+						<configuration>
+							<arguments>install</arguments>
+						</configuration>
+					</execution>
+					<execution>
+						<id>yarn-build</id>
+						<goals>
+							<goal>yarn</goal>
+						</goals>
+						<phase>prepare-package</phase>
+						<configuration>
+							<arguments>run build</arguments>
+						</configuration>
+					</execution>
+				</executions>
 			</plugin>
 		</plugins>
 	</build>
-	<profiles>
-		<profile>
-			<id>optimize</id>
-			<activation>
-				<activeByDefault>true</activeByDefault>
-			</activation>
-			<build>
-				<plugins>
-					<plugin>
-						<groupId>org.apache.maven.plugins</groupId>
-						<artifactId>maven-dependency-plugin</artifactId>
-						<version>2.8</version>
-						<executions>
-							<execution>
-								<id>unpack-dependencies</id>
-								<phase>prepare-package</phase>
-								<goals>
-									<goal>unpack-dependencies</goal>
-								</goals>
-								<configuration>
-									<outputDirectory>${project.build.directory}/requirejs</outputDirectory>
-								</configuration>
-							</execution>
-						</executions>
-					</plugin>
-					<plugin>
-						<groupId>org.fao.unredd</groupId>
-						<artifactId>jwebclient-analyzer-maven-plugin</artifactId>
-EOF
-
-cat >> ${pom} << EOF
-						<version>${GEOLADRIS_VERSION}</version>
-EOF
-
-cat >> ${pom} << 'EOF'
-						<executions>
-							<execution>
-								<id>generate-minified</id>
-								<phase>prepare-package</phase>
-								<goals>
-									<goal>generate-minified</goal>
-								</goals>
-								<configuration>
-									<mainTemplate>${project.build.directory}/requirejs/main.js</mainTemplate>
-									<webClientFolder>${project.build.directory}/requirejs</webClientFolder>
-									<buildconfigOutputPath>${project.build.directory}/buildconfig.js</buildconfigOutputPath>
-									<mainOutputPath>${project.build.directory}/requirejs/geoladris/modules/main.js</mainOutputPath>
-								</configuration>
-							</execution>
-						</executions>
-					</plugin>
-					<plugin>
-						<groupId>ro.isdc.wro4j</groupId>
-						<artifactId>wro4j-maven-plugin</artifactId>
-						<version>1.7.6</version>
-						<executions>
-							<execution>
-								<phase>prepare-package</phase>
-								<goals>
-									<goal>run</goal>
-								</goals>
-							</execution>
-						</executions>
-						<configuration>
-							<wroManagerFactory>ro.isdc.wro.maven.plugin.manager.factory.ConfigurableWroManagerFactory</wroManagerFactory>
-							<extraConfigFile>${basedir}/src/main/config/wro.properties</extraConfigFile>
-							<targetGroups>portal-style</targetGroups>
-							<minimize>true</minimize>
-							<contextFolder>${basedir}/target/requirejs/geoladris/</contextFolder>
-							<destinationFolder>${basedir}/src/main/webapp/optimized/</destinationFolder>
-							<wroFile>${basedir}/src/main/config/wro.xml</wroFile>
-						</configuration>
-					</plugin>
-					<plugin>
-						<groupId>com.github.bringking</groupId>
-						<artifactId>requirejs-maven-plugin</artifactId>
-						<version>2.0.4</version>
-						<executions>
-							<execution>
-								<phase>prepare-package</phase>
-								<goals>
-									<goal>optimize</goal>
-								</goals>
-							</execution>
-						</executions>
-						<configuration>
-							<configFile>${project.build.directory}/buildconfig.js</configFile>
-							<fillDepsFromFolder>${project.build.directory}/requirejs/geoladris/modules</fillDepsFromFolder>
-							<optimizerParameters>
-								<parameter>optimize=uglify2</parameter>
-							</optimizerParameters>
-							<filterConfig>true</filterConfig>
-							<skip>false</skip>
-						</configuration>
-					</plugin>
-				</plugins>
-			</build>
-		</profile>
-	</profiles>
 </project>
 EOF
+
+################
+# package.json #
+################
+echo "[INFO] Generating JavaScript dependency files..."
+
+cat > "${workDir}/package.json" << EOF
+{
+  "name": "${name}",
+  "version": "${version}",
+  "dependencies": {
+    "@geoladris/geojson": "file:/home/vicgonco/workspace/geoladris/plugins/geojson",
+    "@geoladris/core": "file:/home/vicgonco/workspace/geoladris/core",
+    "ui": "file:/home/vicgonco/workspace/geobricks/geoladris-plugins"
+  },
+  "devDependencies": {
+    "requirejs": "^2.1.8"
+  },
+  "scripts": {
+    "build": "gl-build-app.js && r.js -o .requirejs-build.js"
+  }
+}
+EOF
+
+echo >> $log
+echo ">> Installing node and yarn..." >> $log
+mvn -f ${pom} process-resources >> $log
+echo >> $log
+
+set +e
+
+for plugin in ${plugins}; do
+  a=`echo ${plugin} | cut -s -d: -f 1`
+  b=`echo ${plugin} | cut -s -d: -f 2`
+  c=`echo ${plugin} | cut -s -d: -f 3`
+  if [ -z "$a$b$c" ]; then
+    dep="@geoladris/${plugin}@${geoladrisVersion}"
+  else
+    dep=${plugin}
+  fi
+  echo >> $log
+  echo "Adding ${dep} with yarn..." >> $log
+  ${workDir}/node/yarn/dist/bin/yarn add "${dep}" >> $log 2>> $log 
+  if [ $? != 0 ]; then
+    echo "[WARN] Invalid JavaScript dependency: ${dep}"
+  fi
+done
+echo >> $log
+
+set -e
 
 ###########
 # web.xml #
@@ -363,39 +377,19 @@ cat > "${workDir}/src/main/webapp/WEB-INF/web.xml" << 'EOF'
 </web-app>
 EOF
 
-###########
-# wro.xml #
-###########
-cat > "${workDir}/src/main/config/wro.xml" << 'EOF'
-<?xml version="1.0" encoding="UTF-8"?>
-<groups xmlns="http://www.isdc.ro/wro"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.isdc.ro/wro wro.xsd">
-  <group name="portal-style">
-    <css>/styles/**.css</css>
-    <css>/modules/**.css</css>
-    <css>/theme/**.css</css>
-  </group>
-</groups>
-EOF
-
-
-##################
-# wro.properties #
-##################
-cat > "${workDir}/src/main/config/wro.properties" << 'EOF'
-preProcessors=cssDataUri,cssImport,semicolonAppender,cssMinJawr
-postProcessors=
-EOF
-
 #########
 # Build #
 #########
-if [ $skipDeploy = "false" -a -n "${distributionSnapshots}" -a -n "${distributionReleases}" ]; then
-  mvn -f ${workDir} deploy
+echo "[INFO] Packaging..."
+echo "Building with Maven..." >> $log
+if [ $deploy = "true" -a -n "${distributionSnapshots}" -a -n "${distributionReleases}" ]; then
+  mvn -f ${pom} deploy >> $log
 else
-  mvn -f ${workDir} package
+  mvn -f ${pom} package >> $log
 fi
 
 cp ${workDir}/target/*.war ${workDir}/../
 rm -rf ${workDir}
+
+echo "[INFO] Done."
+
