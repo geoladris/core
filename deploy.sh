ba15fd02f2e8e8be63b19f0a9a89af225c56207f
Víctor González <victor.gonzalez@geomati.co>
2018-01-12 13:57:39 +0100

    Travis: Sign artifacts only on deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_f06ce5567527_key -iv $encrypted_f06ce5567527_iv -in ci/signingkey.asc.enc -out ci/signingkey.asc -d
     gpg --import signingkey.asc
-    mvn deploy --settings deploy-settings.xml
+    mvn deploy -P sign --settings deploy-settings.xml
 fi
 
